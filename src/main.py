b510edb043952cf837c3ae180589068d0f1602f2
Wanda Carlson <87515018+wanda-carlson@users.noreply.github.com>
2023-12-07 20:33:26 +0500

    Update src/main.py
    Co-authored-by: Henry R. <87513666+henry-richard@users.noreply.github.com>

--- src/main.py
+++ src/main.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     print("Hello world")
     print("Wanda testing preflight 1.11.5 - migrations")
     print("Wanda testing preflight 1.11.5 - migrations")
-    print("Wanda testing preflight 1.11.5 - migrations")
+    print("Wanda and Henry testing preflight 1.11.5 - migrations")
 
