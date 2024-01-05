d24fba06ea5efb1be3ba7095ba7e0db3f27b1fc1
Wanda Carlson <87515018+wanda-carlson@users.noreply.github.com>
2024-01-05 14:36:01 +0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,4 +4,6 @@ This is the main script for the script.
 if __name__ == "__main__":
     print("Hello world")
     print("Wanda testing preflight 1.11.5 - migrations")
+    print("Wanda testing preflight 1.11.5 - migrations")
+
 
