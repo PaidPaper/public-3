7054d687812604a1e4519c45ccbfdb83b90ce10f
Henry R <87513666+henry-richard@users.noreply.github.com>
2024-01-29 20:17:57 +0500

    Update foo.py

--- src/foo.py
+++ src/foo.py
@@ -7,12 +7,10 @@ class Foo:
     print("Hello foo4")
     print("Hello foo5")
     print("Hello foo5")
-    print("Hello foo5")
-    print("Hello foo21")
-    print("Hello foo5")
     print("Hello foo6")
     print("Hello foo7")
     print("Hello foo8")
     print("Hello foo9")
     print("Hello foo10")
+    print("Hello foo11")
     
