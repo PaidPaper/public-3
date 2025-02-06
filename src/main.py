ce460575680aaa816856783da9bd2137fc588811
Wanda Carlson <87515018+wanda-carlson@users.noreply.github.com>
2025-02-05 22:44:58 -0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     print("Wanda testing preflight 1.11.5 - migrations!")
     print("Wanda testing preflight 1.11.5 - migrations!")
     print('this is python')
-
+    print('is it still python?')
 
